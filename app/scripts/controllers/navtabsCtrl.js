d9aa18ec68e070c711198206eb128a1666e4c22d
Eric Adlam <eadlam@gmail.com>
2014-04-18 14:55:41 +0100

    moved tabs logic from view to a config file

---
+++ app/scripts/controllers/navtabsCtrl.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('SchoolMan')
+  .controller('NavtabsCtrl', function ($scope, $routeParams, Location, TABS) {
+
+    $scope.TABS = TABS;
+    $scope.open = Location.open;
+    $scope.userAccess = $routeParams.accessCode;
+
+    $scope.activeIfPage = function(page){
+      var cssClass = "";
+      if(page === $routeParams.page){
+        cssClass = 'active';
+      }
+      return cssClass;
+    };
+
+    var excludedOnThisPage = function(tab){
+      return tab.exclude.indexOf($routeParams.page) > -1;
+    };
+
+    $scope.userHasAccess = function(tab){
+    	var hasAccess = tab.access.indexOf($scope.userAccess) > -1;
+      var excluded = excludedOnThisPage(tab);
+      return (hasAccess && (!excluded));
+    };
+  });
