2d3ce855af0285a87d6d1f159805fc6e77561641
Eric Adlam <eadlam@gmail.com>
2014-04-22 12:12:21 +0100

    added forms to promotionCtrl so that form selector would work

--- app/scripts/controllers/promotionCtrl.js
+++ app/scripts/controllers/promotionCtrl.js
@@ -1,11 +1,16 @@
 'use strict';
 
 angular.module('SchoolMan')
-  .controller('PromotionCtrl', function ($scope, $routeParams, CourseCatalog) {
+  .controller('PromotionCtrl', function ($scope, $routeParams, CourseCatalog, Location) {
     $scope.formIndex  = $routeParams.formIndex;
+
+    $scope.forms = CourseCatalog.getForms();
+    $scope.form  = $scope.forms[$routeParams.formIndex];
 
     var _groups = CourseCatalog.getGroups();
     $scope.groups = _groups.map(function(group){
     	return angular.copy(group);
     });
+
+    $scope.open = Location.open;
   });
