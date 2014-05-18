31d67bb135ca3f6db510b56c5bdd277f97d08668
Eric Adlam <eadlam@gmail.com>
2014-05-18 14:09:01 +0100

    moved fees over to new db

--- app/scripts/controllers/feesCtrl.js
+++ app/scripts/controllers/feesCtrl.js
@@ -1,26 +1,30 @@
 'use strict';
 
 angular.module('SchoolMan')
-  .controller('FeesCtrl', function ($scope, Fees, Fee) {
-    $scope.fees = Fees.getAll();
-    $scope.newFee = new Fee();
+  .controller('FeesCtrl', function ($scope, Fees, model) {
+      
+      $scope.data = {};
+      $scope.data.fees = Fees.getAll();
+      
+      $scope.newFee = new model.Fee();
 
-    $scope.add = function(fee){
-    	if(fee.isValid()){
-    		try{
-                fee.amount = Number(fee.amount.replace(/[^0-9\.]+/g,""));
-	    		Fees.add(fee);
-	    		Fees.save();
-	    		$scope.newFee = new Fee();	
-	    	} catch(e){
-	    		console.log("FeesCtrl Error: ", e)
-	    	}
-    	}	
-    }
+      $scope.add = function(fee){
+         if(fee.isValid()){
+            try{
+               fee.amount = Number(fee.amount.replace(/[^0-9\.]+/g,""));
+               fee.save().then(function(success){
+                  $scope.data.fees[$scope.newFee._id] = $scope.newFee;
+                  $scope.newFee = new model.Fee(); 
+               }).catch(function(error, result){
+                  console.log("Error: Fee not added", error);
+               });
+           } catch(e){
+               console.log("FeesCtrl Error: ", e)
+           }
+         }  
+      }
 
-    $scope.remove = function(fee){
-    	Fees.remove(fee);	
-        Fees.save();
-
-    }
-  });
+      $scope.remove = function(fee){
+         Fees.remove(fee); 
+      }
+});
