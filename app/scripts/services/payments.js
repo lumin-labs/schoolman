25589823ba1baea6eacb697bd78d793fd7be1dfc
Eric Adlam <eadlam@gmail.com>
2014-05-28 09:58:40 +0100

    started fixing balance sheet

--- app/scripts/services/payments.js
+++ app/scripts/services/payments.js
@@ -6,6 +6,46 @@ angular.module('SchoolMan')
 
   	self = {};
 
+  	self.getAll = function(){
+  		var deferred = $q.defer();
+
+  		var map = function(doc, emit){
+  			var dataModel = model.Payment;
+	      if(doc.datatype === dataModel.datatype._id){
+	      	var obj = model.parse(doc, dataModel.datatype);
+	      	emit(doc._id, {_id:obj.studentId, data:doc});
+	      } 
+	    };
+
+  		Data2.query(map, {include_docs : true}).then(function(success){
+  			  var collection = {};
+  			  var dataModelStudent = model.Student;
+  			  var dataModelPayment = model.Payment;
+  			  angular.forEach(success.rows, function(data, $index){
+  			  	var studentId = data.doc._id;
+  			  	if(!collection.hasOwnProperty(studentId)){
+  			  		var student = model.parse(data.doc, dataModelStudent.datatype);
+  			  		collection[studentId] = {
+  			  			formIndex:student.formIndex,
+  			  			deptId:student.deptId,
+  			  			groupId:student.groupId,
+  			  			payments:[]
+  			  		};
+  			  	};
+  			  	var payment = model.parse(data.value.data, dataModelPayment.datatype);
+  			  	collection[studentId].payments.push(payment);
+  			  });
+	    		console.log("Payments fetched: ", success);
+	    		console.log("Collection started: ", collection);
+	        // deferred.resolve(collection);
+	    }).catch(function(error){
+	        deferred.reject("Query: failed", error);
+	    });
+  		
+  		return deferred.promise;
+
+  	};
+
   	self.query = function(params){
     	
     	var deferred = $q.defer();
