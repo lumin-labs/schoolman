6718b6c28f8b547966e0af5a15a40cf569dcf0ef
Lexie Miller <lexie.j.miller@gmail.com>
2014-07-13 18:46:01 +0100

    school info added

---
+++ app/scripts/services/schoolinfos.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('SchoolMan')
+  .service('SchoolInfos', function SchoolInfos($q, Data2, model) {
+
+  	var self = {};
+
+    var modelFrom = function(data){
+        var spec = model.parse2(data, data.datatype);
+        var info = new model.SchoolInfo(spec);
+        return info;
+    };
+
+  	self.get = function(){
+      var deferred = $q.defer();
+
+  		Data2.get("schoolinfo").then(function(data){
+  			deferred.resolve(modelFrom(data));
+  		}).catch(function(error){
+  			//var info = new model.SchoolInfo();
+        if(error.status === 404){
+          var info = new model.SchoolInfo({nameEn:"GOVERNMENT BILINGUAL HIGH SCHOOL ATIELA-NKWEN",
+                                            nameFr:"LYCEE BILINGUE D'ATIELA-NKWEN"
+                                          });
+          info.save().then(function(success){
+            deferred.resolve(info);
+          }).catch(function(error){
+            deferred.reject(error);
+          })
+        } else{
+          deferred.reject(error);
+        }
+  		});
+
+      return deferred.promise;
+  	};
+
+  	return self;
+
+});
