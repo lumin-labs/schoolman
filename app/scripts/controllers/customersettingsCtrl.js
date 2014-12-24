03c187892eb14eaf1b3cde01690367145c40a2d6
Lexie Miller <lexie.j.miller@gmail.com>
2014-12-24 12:06:07 +0100

    module name bug

--- app/scripts/controllers/customersettingsCtrl.js
+++ app/scripts/controllers/customersettingsCtrl.js
@@ -14,11 +14,11 @@
     if($scope.settings.access.hasOwnProperty(setting)){
       $scope.settings.access[setting] = ($scope.settings.access[setting] + 1) % 2;
     } else if($scope.availableExtensions.indexOf(setting) > -1){
-      var index = $scope.settings.extensions.indexOf(setting.moduleName);
+      var index = $scope.settings.extensions.indexOf(setting.name);
       if(index > -1){
         delete $scope.settings.extensions[index];
       } else {
-        $scope.settings.extensions.push(setting.moduleName);
+        $scope.settings.extensions.push(setting.name);
       }
     }
       
