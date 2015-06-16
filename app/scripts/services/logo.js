0fbbdaa6f10af051bfdb1f8a8708a5e0dd1da5c1
lite04 <lexie.j.miller@gmail.com>
2015-06-16 15:40:10 -0700

    logo upload

--- app/scripts/services/logo.js
+++ app/scripts/services/logo.js
@@ -1,33 +1,57 @@
 'use strict';
 
-function Logo(Data2) {
+function Logo($q, Data2) {
 
     var self = {};
 
     self.save = function(logo){
-        console.log("saving logo", logo);
-       Data2.put(logo).then(function(success){
-        console.log("saved logo", success);
-       }).catch(function(error){
-        console.log("error: save lastUid", error);
-       });
-    };
-
-    self.get = function(){
         var deferred = $q.defer();
 
-        Data2.get("logo").then(function(logo){
-            deferred.resolve(logo);
+        Data2.get("logo").then(function(oldLogo){
+            oldLogo._attachments = logo._attachments;
+           Data2.put(oldLogo).then(function(success){
+            console.log("saved logo", success);
+            deferred.resolve(success);
+           }).catch(function(error){
+            console.log("error: save logo", error);
+            deferred.reject(error);
+           });
         }).catch(function(error){
             console.log("Logo Error", error);
-            
+            if(error.status === 404){
+                Data2.put(logo).then(function(success){
+                    console.log("saved logo", success);
+                    deferred.resolve(success);
+                }).catch(function(error){
+                    console.log("error: save logo", error);
+                    deferred.reject(error);
+                });
+            }else{
+                deferred.reject(error);
+            }
         });
+        return deferred.promise;
+    };
 
+        
+        
+    self.get = function() {
+        var deferred = $q.defer();
+        var img = document.createElement('img');
+        Data2.getAttachment('logo', 'logo.png').then(function(success){
+            var url = URL.createObjectURL(success);
+            img.src = url;
+            img.width = "100";
+            deferred.resolve(img);
+        }).catch(function(error){
+            console.log("Error retreiving attachment", error);
+            deferred.reject(error);
+        })
         return deferred.promise;
     }
 
     return self;
 
   }
-Logo.$inject = ['Data2'];
+Logo.$inject = ['$q','Data2'];
 angular.module('SchoolMan').service('Logo', Logo);
